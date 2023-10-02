b79c4f15e801d89985df36ac296af70ec4478e60
nhatngk <nhat.ngk@gmail.com>
2023-10-02 18:24:11 +0700

    Update taxCalculator

--- src/Week3/Tax.java
+++ src/Week3/Tax.java
@@ -5,17 +5,17 @@ public class Tax {
         double result ;
         if (I <= 0) {
             return -1;
-        } else if (I > 0 && I <= 5) {
+        } else if (I <= 5) {
             result = Math.round(I * 5 / 100.0 * 1000.0) / 1000.0;
-        } else if (I > 5 && I <= 10) {
+        } else if (I <= 10) {
             result = Math.round(I * 10 / 100.0 * 1000.0 - 0.25 * 1000.0) / 1000.0;
-        } else if (I > 10 && I <= 18) {
+        } else if (I <= 18) {
             result = Math.round(I * 15 / 100.0 * 1000.0 - 0.75 * 1000.0) / 1000.0;
-        } else if (I > 18 && I <= 32) {
+        } else if (I <= 32) {
             result = Math.round(I * 20 / 100.0 * 1000.0 - 1.65 * 1000.0) / 1000.0;
-        } else if (I > 32 && I <= 52) {
+        } else if (I <= 52) {
             result = Math.round(I * 25 / 100.0 * 1000.0 - 3.25 * 1000.0) / 1000.0;
-        } else if (I > 52 && I <= 80) {
+        } else if (I <= 80) {
             result = Math.round(I * 30 / 100.0 * 1000.0 - 5.85 * 1000.0) / 1000.0;
         } else {
             result = Math.round(I * 35 / 100.0 * 1000.0 - 9.85 * 1000.0) / 1000.0;
